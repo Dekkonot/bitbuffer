d9785ab96711a2902f8042f4a4fad7617ce3d3f5
Dekkonot <dekkonot@users.noreply.github.com>
2020-05-31 14:58:42 -0700

    Adjust testing library to make it easier to run:  * try.lua is now more readable   * try.lua spec file properly specifies the result of disabled tests

--- src/vanilla/tests/try.lua
+++ src/vanilla/tests/try.lua
@@ -10,6 +10,7 @@ local TEST_FAIL_TEXT = "   [-] %s"
 local TEST_NEEDS_PASS = "   %s NEEDS PASS/FAIL SPECIFICATION"
 
 local SHOW_DISABLED_TESTS = false
+local SHOW_SPECIFICS = true
 
 local function newTry(blockName)
     if type(blockName) ~= "string" then
@@ -25,10 +26,11 @@ local function newTry(blockName)
             error("arg #2 passed to try block must be a function", 2)
         end
         local testEntry = {
-            testName, -- test name
-            nil, -- should succeed
-            true, -- should run
-            testFunc -- the function to run
+            testName = testName, -- test name
+            testFunc = testFunc, -- the function to run
+            shouldSucceed = nil, -- should succeed
+            shouldRun = true, -- should run
+            emitErrors = false,
         }
         tests[testNum] = testEntry
         testNum = testNum+1
@@ -36,19 +38,23 @@ local function newTry(blockName)
         local testControl
         testControl = {
             fail = function()
-                testEntry[2] = false
+                testEntry.shouldSucceed = false
                 return testControl
             end,
             pass = function()
-                testEntry[2] = true
+                testEntry.shouldSucceed = true
                 return testControl
             end,
             disable = function()
-                testEntry[3] = false
+                testEntry.shouldRun = false
                 return testControl
             end,
             enable = function()
-                testEntry[3] = true
+                testEntry.shouldRun = true
+                return testControl
+            end,
+            emit = function()
+                testEntry.emitErrors = true
                 return testControl
             end,
         }
@@ -67,40 +73,43 @@ local function newTry(blockName)
         local passedCount = 0
         local failedCount = 0
         for i, v in ipairs(tests) do
-            local testName = v[1]
-            local shouldSucceed = v[2]
-            local shouldRun = v[3]
-            local funct = v[4]
-            if shouldRun then
-                local success = pcall(funct)
-                if success == shouldSucceed then
-                    resultsText[i] = string.format(TEST_SUCCESS_TEXT, testName)
+            if v.shouldRun then
+                local success, err = pcall(v.testFunc)
+                if success == v.shouldSucceed then
+                    resultsText[i] = string.format(TEST_SUCCESS_TEXT, v.testName)
                     passedCount = passedCount+1
                     finalPassCount = finalPassCount+1
                 else
-                    resultsText[i] = string.format(TEST_FAIL_TEXT, testName)
+                    resultsText[i] = string.format(TEST_FAIL_TEXT, v.testName)
                     failedCount = failedCount+1
                     finalFailCount = finalFailCount+1
                 end
+                if not success and v.emitErrors then
+                    if SHOW_SPECIFICS then
+                        print(string.format("[%s]: %s", v.testName, err))
+                    end
+                end
             else
                 if SHOW_DISABLED_TESTS then
-                    resultsText[i] = string.format(TEST_DISABLED_TEXT, testName)
+                    resultsText[i] = string.format(TEST_DISABLED_TEXT, v.testName)
                 else
                     resultsText[i] = ""
                 end
                 finalDisabledCount = finalDisabledCount+1
             end
-            if shouldSucceed == nil then
-                resultsText[i] = string.format(TEST_NEEDS_PASS, testName)
+            if v.shouldSucceed == nil then
+                resultsText[i] = string.format(TEST_NEEDS_PASS, v.testName)
             end
         end
-        print(string.format(BLOCK_NAME_TEXT, blockName, passedCount, failedCount))
-        for _, v in ipairs(resultsText) do
-            if v ~= "" then
-                print(v)
+        if SHOW_SPECIFICS then
+            print(string.format(BLOCK_NAME_TEXT, blockName, passedCount, failedCount))
+            for _, v in ipairs(resultsText) do
+                if v ~= "" then
+                    print(v)
+                end
             end
+            print("") -- And an extra newline just for readability
         end
-        print("") -- And an extra newline just for readability
         return failedCount == 0
     end
 
